d3f4f58303838b143353039a712a33ef2c72bf1d
Evelyn <evelinagudelosepulveda@gmail.com>
2026-04-24 23:43:30 -0500

    Corrigiendo errores y creando estilos

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,23 @@
 import "./Hero.css";
 
-function Hero(){
-    return(
-        <main className="hero-section">
-            <div className="hero-content">
-                <h1>El arte de la paciencia</h1>
-                <p>Descubre la serenidad a través del cuidado y diseño de árboles Bonsái. Un espacio para reconectar con la naturaleza.</p>
-                <a href="#" className="btn">
-                    Reserva un taller
-                </a>
-            </div>
-            <div className="hero-image"></div>
-        </main>
-    );
+function Hero() {
+  return (
+    <main className="hero-section">
+      <div className="hero-content">
+        <h1>El arte de la paciencia</h1>
+        <p>
+          Descubre la serenidad a través del cuidado y diseño de árboles Bonsái.
+          Un espacio para reconectar con la naturaleza.
+        </p>
+
+        <a href="#" className="btn">
+          Reserva un taller
+        </a>
+      </div>
+
+      <div className="hero-image"></div>
+    </main>
+  );
 }
 
 export default Hero;
